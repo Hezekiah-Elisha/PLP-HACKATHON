a9e4c30fb7a877573ea6f31af178b35f1c7f04b7
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-07-09 23:01:27 +0300

    :memo: PLP Hackathon

---
+++ Day_3_Task_2.py
@@ -0,0 +1,6 @@
+Fats = int(input('type in fat content in grams.'))
+Carbs = int(input('type in carbohydrate content in grams.'))
+Fats_calories = Fats * 9  
+Carbs_calories = Carbs * 4
+print (Fats_calories)
+print (Carbs_calories)
